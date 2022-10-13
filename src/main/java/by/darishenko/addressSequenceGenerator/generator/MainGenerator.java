3985ca72f64d1c7a152e1610e8acd7f18f89c182
darishenko <dar1shenko@yandex.com>
2022-10-13 10:15:39 +0300

    added main generator(until for square generating matrix)

---
+++ src/main/java/by/darishenko/addressSequenceGenerator/generator/MainGenerator.java
@@ -0,0 +1,51 @@
+package by.darishenko.addressSequenceGenerator.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainGenerator {
+
+    private int length;
+    private int initialState;
+
+    public MainGenerator(int length, int initialState) {
+        this.length = length;
+        this.initialState = initialState;
+    }
+
+    public MainGenerator(int length) {
+        this.length = length;
+        this.initialState = 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getInitialState() {
+        return initialState;
+    }
+
+    public void setInitialState(int initialState) {
+        this.initialState = initialState;
+    }
+
+    public List<Integer> generateSequence(List<Integer> generatingMatrix) {
+        List<Integer> result = new ArrayList<>();
+        SwitchingSequenceGenerator switchingSequenceGenerator = new SwitchingSequenceGenerator(length);
+        List<Integer> switchMatrix = switchingSequenceGenerator.generateSequence();
+        int currState = initialState;
+
+        result.add(currState);
+        for (int i = 0; i < Math.pow(2, length) - 1; i++) {
+            currState = currState ^ generatingMatrix.get(switchMatrix.get(i));
+            result.add(currState);
+        }
+
+        return result;
+    }
+}
